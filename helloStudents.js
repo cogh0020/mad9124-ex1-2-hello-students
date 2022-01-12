201321ad087c79bb3998945ff53fe132116b8eed
--replace-all <cogh0020@algonquinlive.com>
2022-01-11 19:38:03 -0500

    Finished exercise

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,18 @@
 'use strict'
 
 // 1. Read the JSON file into a variable called students
+let students = require('./students.json')
 
 // 2. Iterate over the students array and print Hello with their full names to the console
+students.forEach((student) => {
+    console.log(`Hello ${student.firstName} ${student.lastName}`)
+})
 // e.g. Hello Walter Baker
 
 // 3. Print out the number of last names starting with the letter D
+
+const counter = students.filter(student => {
+    return student.lastName.charAt(0) === 'D'
+}) 
+console.log(`People with last names starting with D: ${counter.length}`)
 // e.g. Count of last names starting with D is 1
